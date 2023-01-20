4ef825f7f5b84272ac097b6181e01d03cb61242a
Rishav Choudhary <78720189+rishav015@users.noreply.github.com>
2023-01-20 17:19:41 +0530

    Add files via upload

---
+++ Online_Banking_System/src/com/utility/DBUtil.java
@@ -0,0 +1,38 @@
+package com.onlinebanking.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	
+	public static Connection provideConnection() {
+		
+		//standard procedure
+		
+		Connection connect =null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String url ="jdbc:mysql://localhost:3306/sb101db";
+		
+		try {
+			connect = DriverManager.getConnection(url, "root", "9736043994@Ri");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return connect;
+		
+		
+		
+		
+	}
+
+}
